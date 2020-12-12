a7a821bb213320cf7b12f9a6095f19a2d896dd41
niki4 <nikiforr@gmail.com>
2020-12-12 16:59:21 +0300

    Add tests for Is Palindrome Num

---
+++ easy/009_palindrome_number_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+var palindromeNumTC = []struct {
+	input  int
+	output bool
+}{
+	{121, true},
+	{-121, false},
+	{10, false},
+	{-101, false},
+}
+
+func TestIsPalindrome(t *testing.T) {
+	for _, tc := range palindromeNumTC {
+		t.Run(fmt.Sprint(tc.input), func(t *testing.T) {
+			if res := isPalindrome(tc.input); res != tc.output {
+				t.Errorf("want %t, got %t", tc.output, res)
+			}
+		})
+	}
+}
