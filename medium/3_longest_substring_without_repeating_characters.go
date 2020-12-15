98c83b458834344696de5e490c4c76f92fc340b3
niki4 <nikiforr@gmail.com>
2020-12-15 14:21:30 +0300

    Add O(n) solution for Longest Substring Without Repeating Characters

--- medium/3_longest_substring_without_repeating_characters.go
+++ medium/3_longest_substring_without_repeating_characters.go
@@ -11,6 +11,8 @@ Explanation: The answer is "abc", with the length of 3.
 
 // Runtime: 228 ms, faster than 16.91% of Go
 // Memory Usage: 6.5 MB, less than 17.54% of Go
+// Time complexity: O(n**2)
+// Space complexity: O(n)
 func lengthOfLongestSubstring(s string) int {
 	longestUniqueLen := 0
 	str := []rune(s)
@@ -32,4 +34,24 @@ func lengthOfLongestSubstring(s string) int {
 		}
 	}
 	return longestUniqueLen
+}
+
+// Runtime: 8 ms, faster than 67.23% of Go
+// Memory Usage: 3.1 MB, less than 64.78% of Go
+// Time complexity: O(n)
+// Space complexity: O(n)
+func lengthOfLongestSubstring2(s string) int {
+	str := []rune(s)
+	set := make(map[rune]struct{})
+	ans, i, j := 0, 0, 0
+	for i < len(str) && j < len(str) {
+		if _, exist := set[str[j]]; !exist {
+			set[str[j]] = struct{}{}
+			j++
+		} else {
+			delete(set, str[i])
+			i++
+		}
+	}
+	return ans
 }
