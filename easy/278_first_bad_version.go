180fd72116957cce758d017248c779b1a7bad1c3
niki4 <nikiforr@gmail.com>
2020-12-24 18:58:50 +0300

    Add solution for First Bad Version

---
+++ easy/278_first_bad_version.go
@@ -0,0 +1,53 @@
+/**
+You are a product manager and currently leading a team to develop a new product.
+Unfortunately, the latest version of your product fails the quality check.
+Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one,
+which causes all the following ones to be bad.
+
+You are given an API bool isBadVersion(version) which returns whether version is bad.
+Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+Example 1:
+Input: n = 5, bad = 4
+Output: 4
+Explanation:
+call isBadVersion(3) -> false
+call isBadVersion(5) -> true
+call isBadVersion(4) -> true
+Then 4 is the first bad version.
+
+Example 2:
+Input: n = 1, bad = 1
+Output: 1
+*/
+
+package main
+
+/**
+ * Forward declaration of isBadVersion API.
+ * @param   version   your guess about first bad version
+ * @return 	 	      true if current version is bad
+ *			          false if current version is good
+ * func isBadVersion(version int) bool;
+ */
+func isBadVersion(version int) bool {
+	return true // Simple plug, real implementation provided by LC and unknown.
+}
+
+// Binary search
+// Runtime: 0 ms, faster than 100.00% of Go
+// Memory Usage: 1.9 MB, less than 100.00% of Go
+func firstBadVersion(n int) int {
+	left, right := 1, n
+	for left < right {
+		mid := left + (right-left)/2
+		if isBadVersion(mid) {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+	return left
+}
