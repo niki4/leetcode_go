74106a4bed10fd683c6756a7aac62fc75a208fc3
niki4 <nikiforr@gmail.com>
2021-01-13 18:09:03 +0300

    Add solution for Fizz Buzz

---
+++ easy/412_fizz_buzz.go
@@ -0,0 +1,30 @@
+/**
+Write a program that outputs the string representation of numbers from 1 to n.
+
+But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”.
+For numbers which are multiples of both three and five output “FizzBuzz”.
+*/
+
+package main
+
+import "strconv"
+
+// Runtime: 4 ms, faster than 97.62% of Go
+// Memory Usage: 3.4 MB, less than 100.00% of Go
+func fizzBuzz(n int) []string {
+	res := make([]string, n)
+	var val string
+	for i := 1; i < n+1; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			val = "FizzBuzz"
+		} else if i%3 == 0 {
+			val = "Fizz"
+		} else if i%5 == 0 {
+			val = "Buzz"
+		} else {
+			val = strconv.Itoa(i)
+		}
+		res[i-1] = val
+	}
+	return res
+}
---
+++ easy/412_fizz_buzz_test.go
@@ -0,0 +1,32 @@
+package main
+
+import "testing"
+
+func TestFizzBuzz(t *testing.T) {
+	exp := []string{
+		"1",
+		"2",
+		"Fizz",
+		"4",
+		"Buzz",
+		"Fizz",
+		"7",
+		"8",
+		"Fizz",
+		"Buzz",
+		"11",
+		"Fizz",
+		"13",
+		"14",
+		"FizzBuzz",
+	}
+	res := fizzBuzz(15)
+	if len(res) != len(exp) {
+		t.Fatalf("Expected len %d, got %d: %#v", len(exp), len(res), res)
+	}
+	for i, v := range exp {
+		if res[i] != v {
+			t.Errorf("Mismatch at index %d: want %s, got %v", i, v, res[i])
+		}
+	}
+}
