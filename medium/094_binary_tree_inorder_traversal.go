b1e69b13afbed0a94051693cb071d18defb84a4d
niki4 <nikiforr@gmail.com>
2021-02-02 15:15:59 +0300

    Add recursive approach

--- medium/094_binary_tree_inorder_traversal.go
+++ medium/094_binary_tree_inorder_traversal.go
@@ -47,3 +47,21 @@ func inorderTraversal(root *TreeNode) []int {
 		node = node.Right
 	}
 }
+
+// Runtime: 0 ms, faster than 100.00% of Go
+// Memory Usage: 2.1 MB, less than 31.18% of Go
+// Time and Space complexity: O(n)
+func inorderTraversalRecursive(root *TreeNode) []int {
+	res := new([]int)
+	inorderHelper(res, root)
+	return *res
+}
+
+func inorderHelper(res *[]int, node *TreeNode) {
+	if node == nil {
+		return
+	}
+	inorderHelper(res, node.Left)
+	*res = append(*res, node.Val)
+	inorderHelper(res, node.Right)
+}
