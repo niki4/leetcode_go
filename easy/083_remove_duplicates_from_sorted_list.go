f3d742894fd2c331bff09727ac4a6b3df62e0769
niki4 <nikiforr@gmail.com>
2021-05-20 18:28:51 +0300

    Add "Remove Duplicates from Sorted List" solution

---
+++ easy/083_remove_duplicates_from_sorted_list.go
@@ -0,0 +1,17 @@
+package main
+
+// Runtime: 0 ms, faster than 100.00% of Go
+// Memory Usage: 3.1 MB, less than 100.00% of Go
+// Time complexity: O(n) because we need visit all nodes, remove dup node takes O(1)
+// Space complexity: O(1)
+func deleteDuplicates(head *ListNode) *ListNode {
+	curr := head
+	for curr != nil && curr.Next != nil {
+		if curr.Val == curr.Next.Val { // found dup
+			curr.Next = curr.Next.Next
+		} else {
+			curr = curr.Next
+		}
+	}
+	return head
+}
