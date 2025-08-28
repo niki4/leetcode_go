8e8074a60d2c522aaa4fa2d24efebee5861aa677
Ivan Nikiforov <nikiforr@gmail.com>
2025-08-28 14:45:17 +0300

    Create 0042_trapping-rain-water.go

---
+++ hard/0042_trapping-rain-water.go
@@ -0,0 +1,46 @@
+/*
+Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+
+Example 1:
+https://assets.leetcode.com/uploads/2018/10/22/rainwatertrap.png
+
+Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+Output: 6
+Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1].
+In this case, 6 units of rain water (blue section) are being trapped.
+
+Example 2:
+Input: height = [4,2,0,3,2,5]
+Output: 9 
+
+Constraints:
+
+n == height.length
+1 <= n <= 2 * 104
+0 <= height[i] <= 105
+*/
+
+package hard
+
+// Two-pointer approach
+// Time complexity: O(n)
+// Space complexity: O(1)
+func trap(height []int) int {
+    left, right := 0, len(height)-1
+    maxLeft, maxRight := 0, 0
+    maxArea := 0
+
+    for left < right {
+        if height[left] < height[right] {
+            maxLeft = max(maxLeft, height[left])
+            maxArea += maxLeft - height[left]
+            left++
+        } else {
+            maxRight = max(maxRight, height[right])
+            maxArea += maxRight - height[right]
+            right--
+        }
+    }
+
+    return maxArea
+}
